1ccca9ae12711a7ce8139c50a831703485c76637
Avi Samloff <avi.samloff@gmail.com>
2015-09-28 20:23:42 -0700

    Major Update
    Added random coin. Timing is set to 2 sec, need to adjust.

--- js/main.js
+++ js/main.js
@@ -1,78 +1,111 @@
-/* Starting Data */
-var inventory = 0;
-var clickMultiplier = 1;
-var costClickMultiplier = 10;
-var hasAutoClick1 = false;
-var costAutoClick1 = 1000;
+$(document).ready(function() {
+   /* Starting Data */
+   var inventory = 0;
+   var clickMultiplier = 1;
+   var costClickMultiplier = 10;
+   var hasAutoClick1 = false;
+   var costAutoClick1 = 1000;
 
-/************** God Mode **************/
-var godMode = true;
+   /************** v God Mode v **************/
+   var godMode = true;
 
-if (godMode) {
-   console.log('God Mode Active');
-   $('#godmode').show();
-   inventory = 999999999999;
-}
-/************** God Mode **************/
+   if (godMode) {
+      console.log('God Mode Active');
+      $('#godmode').show();
+      inventory = 999999999999;
+   }
+   /************** ^ God Mode ^ **************/
 
 
 
-/* Functions */
-var dispInventory = function() {
-   $('.inventory').text('Coins: ' + inventory);
-};
-var dispClickMultiplier = function() {
-   $('.click-multiplier').text('Click Multiplier: ' + clickMultiplier);
-};
-var dispClickMultiplierCost = function() {
-   $('.increase-multiplier-cost').text('Cost: ' + costClickMultiplier + ' ');
-};
-var dispAutoClick1Cost = function() {
-   $('.auto-click-1-cost').text('Cost: ' + costAutoClick1 + ' ');
-};
-var dispAutoClick1 = function() {
-   $('.has-auto-click-1').text('Auto Click 1: ' + hasAutoClick1 + ' ');
-};
+   /* Functions */
+   var dispInventory = function() {
+      $('.inventory').text('Coins: ' + inventory);
+   };
+   var dispClickMultiplier = function() {
+      $('.click-multiplier').text('Click Multiplier: ' + clickMultiplier);
+   };
+   var dispClickMultiplierCost = function() {
+      $('.increase-multiplier-cost').text('Cost: ' + costClickMultiplier + ' ');
+   };
+   var dispAutoClick1Cost = function() {
+      $('.auto-click-1-cost').text('Cost: ' + costAutoClick1 + ' ');
+   };
+   var dispAutoClick1 = function() {
+      $('.has-auto-click-1').text('Auto Click 1: ' + hasAutoClick1 + ' ');
+   };
 
-/* Display All Info */
-dispInventory();
-dispClickMultiplier();
-dispClickMultiplierCost();
-dispAutoClick1Cost();
-
-/* User Click Function */
-$('.click').on('click', function() {
-   inventory += 1 * clickMultiplier;
+   /* Display All Info */
    dispInventory();
-});
+   dispClickMultiplier();
+   dispClickMultiplierCost();
+   dispAutoClick1Cost();
 
-/* AutoClick1 Function */
-var autoClick = function() {
-   if (hasAutoClick1) {
+   /* User Click Function */
+   $('.click').on('click', function() {
+      inventory += 1 * clickMultiplier;
+      dispInventory();
+   });
+
+   /* AutoClick1 Function */
+   var autoClick = function() {
+      if (hasAutoClick1) {
+         inventory++;
+         dispInventory();
+      }
+   };
+   setInterval(autoClick, 1000);
+
+   /**** Store Functions ****/
+   $('.increase-multiplier').click(function() {
+      if (inventory >= costClickMultiplier) {
+         inventory -= costClickMultiplier;
+         clickMultiplier += 1;
+         costClickMultiplier = costClickMultiplier * clickMultiplier;
+         dispInventory();
+         dispClickMultiplier();
+         dispClickMultiplierCost();
+      }
+   });
+
+   $('.auto-click-1').click(function() {
+      if (inventory >= costAutoClick1) {
+         inventory -= costAutoClick1;
+         hasAutoClick1 = true;
+         dispInventory();
+         dispAutoClick1();
+         $('.auto-click-1-wrapper').hide();
+      }
+   });
+
+   /* Random Coin */
+   function generateRandomWidth() {
+      var result = Math.floor(Math.random() * $(window).width());
+      while (result > $(window).width()) {
+         result = Math.floor(Math.random() * $(window).width());
+      }
+      return result;
+   }
+   function generateRandomHeight() {
+      var result = Math.floor(Math.random() * $(window).height());
+      while (result > $(window).width()) {
+         result = Math.floor(Math.random() * $(window).height());
+      }
+      return result;
+   }
+   function showCoin() {
+      $(".container").append('<img class="spinning-coin-image" src="img/coin-spinning-transparent.gif">');
+      var left = generateRandomWidth();
+      var top = generateRandomHeight();
+      $('.spinning-coin-image').last().css({"position": "absolute", "top": top, "left": left});
+   }
+
+   setInterval(showCoin, 2000);
+
+   $('.container').on('click', '.spinning-coin-image', function(){
       inventory++;
       dispInventory();
-   }
-};
-setInterval(autoClick, 1000);
+      $(this).hide();
+   });
 
-/* Store Functions */
-$('.increase-multiplier').click(function() {
-   if (inventory >= costClickMultiplier) {
-      inventory -= costClickMultiplier;
-      clickMultiplier += 1;
-      costClickMultiplier = costClickMultiplier * clickMultiplier;
-      dispInventory();
-      dispClickMultiplier();
-      dispClickMultiplierCost();
-   }
-});
-
-$('.auto-click-1').click(function() {
-   if (inventory >= costAutoClick1) {
-      inventory -= costAutoClick1;
-      hasAutoClick1 = true;
-      dispInventory();
-      dispAutoClick1();
-      $('.auto-click-1-wrapper').hide();
-   }
 });
